04f14c0349ff45f85f8b77bc2743fa5c23fc5e0e
Tanveer Inamdar <tinamdar@ttu.edu>
2025-12-16 21:45:26 -0600

    Steamlit interface

--- interface.py
+++ interface.py
@@ -3,10 +3,24 @@ import pandas as pd
 import numpy as np
 from main import get_response
 
-if prompt := st.chat_input("Enter Smth"):
+if "query" not in st.session_state:
+    st.session_state.query = []
+
+for msg in st.session_state.query:  # Initialize this for each call
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input("Enter something"):
+    # Store user message
+    st.session_state.query.append(
+        {"role": "user", "content": prompt}
+    )
+
     st.chat_message("user").write(prompt)
     with st.spinner("Hold on..."):
-        result = get_response(str(prompt))
+        result = get_response(st.session_state.query)
 
-    st.chat_message("Assistant: ").write(result)
+    st.session_state.query.append(
+        {"role": "assistant", "content": result}
+    )
 
+    st.chat_message("assistant").write(result)
--- main.py
+++ main.py
@@ -3,23 +3,20 @@ import requests
 import json
 from dotenv import load_dotenv
 load_dotenv()
-def get_response(prompt: str):
+def get_response(message):
   OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
   response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
       "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+      "Content-Type": "application/json",
     },
-    data=json.dumps({
+    json={
       "model": "openai/gpt-oss-20b:free",
-      "messages": [
-        {
-          "role": "user",
-          "content": f"{prompt}"
-        }
-      ]
-    })
+      "messages" : message,
+    }
   )
+
   res = (response.json()["choices"][0]["message"]["content"])
   return res
 
