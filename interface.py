50e6f1787c3e8b077fc847d7ce0b86ce9255aca7
Tanveer Inamdar <tinamdar@ttu.edu>
2025-12-17 19:55:07 -0600

    Fixed memory echo bug

--- interface.py
+++ interface.py
@@ -14,22 +14,22 @@ conn = sqlite3.connect("chat_history.db")
 cursor = conn.cursor()
 insert_query = f"""INSERT INTO messages(session_id, role, content) VALUES ("session_id1",?,?)"""
 
-if "query" not in st.session_state:
-    st.session_state.query = []
-cursor.execute('SELECT role, content FROM messages WHERE session_id = ?', ("session_id1",))
-rows = cursor.fetchall()
-for row in rows:
-        # row[0] is role, row[1] is content
-        st.session_state.query.append({"role": row[0], "content": row[1]})
-
-for msg in st.session_state.query:  # Initialize this for each call
-    st.chat_message(msg["role"]).write(msg["content"])
-
 if st.button("Clear current chat", width=100):
     cursor.execute("DELETE FROM messages where session_id = ? ", ("session_id1",))
     conn.commit()
     st.session_state.query = []
     st.rerun()
+
+cursor.execute('SELECT role, content FROM messages WHERE session_id = ?', ("session_id1",))
+rows = cursor.fetchall()
+if "query" not in st.session_state:
+    st.session_state.query = []
+    for row in rows:
+        # row[0] is role, row[1] is content
+        st.session_state.query.append({"role": row[0], "content": row[1]})
+
+for row in rows:
+    st.chat_message(row[0]).write(row[1])
 
 if prompt := st.chat_input("Enter something"):
 
