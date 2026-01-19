5139f4ac76ce26dccb2b686148705bcd4f56e071
Tanveer Inamdar <tinamdar@ttu.edu>
2026-01-19 06:34:58 -0600

    Changed to process chat title first so we won't need multiple reruns on the frontend.

--- worker.py
+++ worker.py
@@ -41,29 +41,33 @@ def backend_worker():
                 conn = sqlite3.connect(DB_FILE, timeout=10)
                 conn.execute("PRAGMA journal_mode=WAL;")
                 cursor = conn.cursor()
+                chat_title = cursor.execute("SELECT title from sessions WHERE session_id = ?", (current_session_id,))
+
+                title_result = chat_title.fetchall()[0][0]
+                conn.close()
+                print(f"Worker Completed for chat title {title_result}")
+                if title_result == "New Chat":
+                    x = new_chat(query)
+                    DB_FILE = "chat_history.db"
+                    conn = sqlite3.connect(DB_FILE)
+                    conn.execute("PRAGMA journal_mode=WAL;")
+                    cursor = conn.cursor()
+                    cursor.execute("UPDATE sessions set title = ? where session_id = ?", (x, current_session_id,))
+                    conn.commit()
+                    print("title changed to ", x)
+                    conn.close()
+
+                DB_FILE = "chat_history.db"
+                conn = sqlite3.connect(DB_FILE, timeout=10)
+                conn.execute("PRAGMA journal_mode=WAL;")
+                cursor = conn.cursor()
                 cursor.execute("UPDATE messages SET state = 'Completed' WHERE id = ?", (row[0],))
                 cursor.execute(insert_query, (current_session_id, "assistant", result, "Completed"))
                 conn.commit()
                 conn.close()
                 print(f"Worker Completed for message id {row[0]}")
 
-                DB_FILE = "chat_history.db"
-                conn = sqlite3.connect(DB_FILE, timeout=10)
-                conn.execute("PRAGMA journal_mode=WAL;")
-                cursor = conn.cursor()
-                chat_title = cursor.execute("SELECT title from sessions WHERE session_id = ?", (current_session_id,))
 
-                title_result = chat_title.fetchall()[0][0]
-                conn.close()
-                print("title changed")
-                if title_result == "New Chat":
-                    x = new_chat(query)
-                    DB_FILE = "chat_history.db"
-                    conn = sqlite3.connect(DB_FILE, timeout=10)
-                    conn.execute("PRAGMA journal_mode=WAL;")
-                    cursor = conn.cursor()
-                    cursor.execute("UPDATE sessions set title = ? where session_id = ?", (x, current_session_id,))
-                    conn.close()
             else:
                 conn.close()
                 time.sleep(1)
