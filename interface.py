937f7bbf241d582622531df49e377563c42c04da
Tanveer Inamdar <tinamdar@ttu.edu>
2026-01-28 17:21:36 -0600

    Steamlit UI with option to choocse the model. (Not connected to the backend.

--- interface.py
+++ interface.py
@@ -7,6 +7,7 @@ from db_init import create_db
 import uuid
 import random
 import time
+from model_list import final_models
 
 
 if "session_id" not in st.session_state:
@@ -14,20 +15,66 @@ if "session_id" not in st.session_state:
 current_session_id = st.session_state.session_id
 
 DB_FILE = "chat_history.db"
-conn = sqlite3.connect(DB_FILE)
-
-cursor = conn.cursor()
 
 welcome_statements = ["Hey Tan, what's on your mind? ", "Hello, Tanveer", "What's up", "Greetings!", "Howdy!"]
 x = random.choice(welcome_statements)
 st.title(f"{x}")
-
-cursor.execute(f"INSERT OR IGNORE INTO sessions(session_id, title) VALUES (?, ?)", (current_session_id, "New Chat"))
+#
+# conn = sqlite3.connect(DB_FILE)
+# cursor = conn.cursor()
+# cursor.execute(f"INSERT OR IGNORE INTO sessions(session_id, title) VALUES (?, ?)", (current_session_id, "New Chat"))
+# conn.commit()
 
 insert_query = f"""INSERT INTO messages(session_id, role, content, state) VALUES (?,?,?,?)"""
-
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
 cursor.execute('SELECT role, content FROM messages WHERE session_id = ? ', (current_session_id,))
 rows = cursor.fetchall()
+conn.close()
+
+with st.sidebar:
+
+    if st.button("Clear current chat", width=100):
+        DB_FILE = "chat_history.db"
+        conn = sqlite3.connect(DB_FILE)
+
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM sessions WHERE session_id = ?", (current_session_id,))
+        cursor.execute("DELETE FROM messages where session_id = ? ", (current_session_id,))
+        conn.commit()
+        st.session_state.query = []
+        #st.session_state.session_id = str(uuid.uuid4())
+        conn.commit()
+        conn.close()
+        st.rerun()
+    if st.button("New Chat"):
+        st.session_state.session_id = str(uuid.uuid4())
+
+        current_session_id = st.session_state.session_id
+        if "query" in st.session_state:
+            st.session_state.query = []
+        st.rerun()
+
+    st.divider()
+    st.title("Past Chats")
+    st.divider()
+
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT title, session_id FROM sessions ORDER BY created_at DESC")
+    past_chat_IDs = cursor.fetchall()
+    conn.close()
+    for ID in past_chat_IDs:
+        chat_title = ID[0]
+        unique_chat_key = ID[1]
+        if st.button(label=chat_title, key=unique_chat_key):
+            st.session_state.session_id = unique_chat_key
+            if "query" in st.session_state:
+                st.session_state.query = []
+            conn.close() #Manual Close as if the code hits this block, it never reaches the script end and never terminates connection
+            st.rerun()
+
+
 if "query" not in st.session_state:
     st.session_state.query = []
     for row in rows:
@@ -40,12 +87,25 @@ for row in rows:
 
 if prompt := st.chat_input("Enter something"):
 
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT OR IGNORE INTO sessions(session_id, title) VALUES (?, ?)", (current_session_id, "New Chat"))
+    conn.commit()
+    insert_query = f"""INSERT INTO messages(session_id, role, content, state) VALUES (?,?,?,?)"""
+
+    cursor.execute('SELECT role, content FROM messages WHERE session_id = ? ', (current_session_id,))
+    rows = cursor.fetchall()
+    conn.close()
+
     # Store user message
     st.session_state.query.append(
         {"role": "User", "content": prompt}
     )
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
     cursor.execute(insert_query,(current_session_id,"User", prompt, "Pending"))
     conn.commit()
+    conn.close()
 
     st.chat_message("User").write(prompt)
     with st.spinner("Hold on..."):
@@ -61,36 +121,14 @@ if prompt := st.chat_input("Enter something"):
             time.sleep(0.1)
             conn.close()
         st.rerun()
+model_choice = st.selectbox(
+        "What model should we use?",
+        final_models,
+        index=None,
+        placeholder="Select AI model...",
 
+    )
 
-with st.sidebar:
-    cursor.execute("SELECT title, session_id FROM sessions ORDER BY created_at DESC")
-    past_chat_IDs = cursor.fetchall()
-    for ID in past_chat_IDs:
-        chat_title = ID[0]
-        unique_chat_key = ID[1]
-        if st.button(label=chat_title, key=unique_chat_key):
-            st.session_state.session_id = unique_chat_key
-            if "query" in st.session_state:
-                st.session_state.query = []
-            conn.close() #Manual Close as if the code hits this block, it never reaches the script end and never terminates connection
-            st.rerun()
+st.write("You selected:", model_choice)
 
-    st.title("Past Chats")
-    if st.button("Clear current chat", width=100):
-        cursor.execute("DELETE FROM sessions WHERE session_id = ?", (current_session_id,))
-        cursor.execute("DELETE FROM messages where session_id = ? ", (current_session_id,))
-        conn.commit()
-        st.session_state.query = []
-        conn.commit()
-        conn.close()
-        st.rerun()
-    if st.button("New Chat"):
-        st.session_state.session_id = str(uuid.uuid4())
-
-        current_session_id = st.session_state.session_id
-        if "query" in st.session_state:
-            st.session_state.query = []
-        conn.close()
-        st.rerun()
 conn.close()
